01eafd856a6c6d308a5890f1682b55abb174c547
qudwo09@naver.com <qudwo09@naver.com>
2021-02-14 21:01:46 +0900

    make video item

--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Videoitem from '../video_item/video_item';
+import styles from './video_list.module.css';
+
 
 const Videolist = (props) => {
-    return <ul>
+    return <ul className={styles.videos}>
         {props.videos.map(video=><Videoitem video={video}/>)}
     </ul>
 }
